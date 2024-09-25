d2a0a42a69cbf273f4a9c264b4ffa5d2f3c10e41
Alex Hansen <hansen.alex@telia.com>
2024-09-25 16:22:03 +0200

    abtsracted db uri

--- server.ts
+++ server.ts
@@ -7,13 +7,49 @@ dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
-mongoose.connect("mongodb+srv://fjs23.fe21v.mongodb.net/industry_management_system", {
+mongoose.connect(process.env.DB_URI as string, {
     user: process.env.DB_USER,
     pass: process.env.DB_PASS
 })
 
-app.get("/", (request: Request, response: Response) => {
-    response.status(200).send();
+app.get("/api/products", (request: Request, response: Response) => {
+    try {
+        response.status(200).send();
+    } catch (error) {
+        response.status(500).send();
+    }
+});
+
+app.get("/api/products/:id", (request: Request, response: Response) => {
+    try {
+        response.status(200).send();
+    } catch (error) {
+        response.status(500).send();
+    }
+});
+
+app.post("/api/products", (request: Request, response: Response) => {
+    try {
+        response.status(200).send();
+    } catch (error) {
+        response.status(500).send();
+    }
+});
+
+app.put("/api/products/:id", (request: Request, response: Response) => {
+    try {
+        response.status(200).send();
+    } catch (error) {
+        response.status(500).send();
+    }
+});
+
+app.delete("/api/products/:id", (request: Request, response: Response) => {
+    try {
+        response.status(200).send();
+    } catch (error) {
+        response.status(500).send();
+    }
 });
 
 app.listen(PORT, () => {
